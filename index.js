e0ac406b8cf79eb05f308a6547367d211cb08420
Wageh Saad <36130281+wagehsaad@users.noreply.github.com>
2021-12-02 17:06:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+$(".imagelayer1,").css("height",$(window).height());
+})
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
